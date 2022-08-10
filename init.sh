b0fddb85546fc7a9cb24312b8971628330285886
lingwh <LRwq942133>
2022-08-10 23:28:23 +0800

    测试

--- init.sh
+++ init.sh
@@ -39,7 +39,7 @@ function init() {
                 #如果代码还没有被下载过，执行执行下载代码的操作
                 git clone $GIT_REPOSITORY_URL $MD_FILE_RELATIVE_PATH/$PROJECT_NAME
                 #写入commit-message
-                curl -X GET --header 'Content-Type: application/json;charset=UTF-8' 'https://gitee.com/api/v5/repos/lingwh1995/'"$PROJECT_NAME"'/commits/master?access_token='"$GITEE_TOKEN'' > $MD_FILE_RELATIVE_PATH/$PROJECT_NAME/commit.txt
+                curl -X GET --header 'Content-Type: application/json;charset=UTF-8' 'https://gitee.com/api/v5/repos/lingwh1995/'"$PROJECT_NAME"'/commits/master?access_token='"$GITEE_TOKEN"'' > $MD_FILE_RELATIVE_PATH/$PROJECT_NAME/commit.txt
             else
                 #如果存在该文件夹，则说明代码已经被下载过了，则对比最新的提交信息和当前项目中已经有的提交信息，看是否一致，不一致说明项目有新提交，需要重新下载
                 echo '代码已经下载过了.....................................'
